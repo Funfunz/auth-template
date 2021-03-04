e8a9f44f2bfa101060e4dfeca3ac3f8a5deabccc
João Leite <joao.leite@isobar.com>
2021-03-04 19:53:07 +0000

    updated funfunz/core

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -5,17 +5,17 @@ import sha512 from "@root/utils/sha512"
 
 const log = logger('hooks/user')
 
-export function hook_addAndUpdateUser(options: IHookProps<null>) {
+export function hook_addAndUpdateUser(options: IHookProps<null, unknown>) {
   if ((options.args.data as any)?.password) {
     (options.args.data as any).password = sha512((options.args.data as any).password)
   }
   return options
 }
 
-export function hook_queryUser(options: IHookProps<null>) {
-  if (options.superUser) {
+export function hook_queryUser(options: IHookProps<null, unknown>) {
+  /*if (options.superUser) {
     return options
-  }
+  }*/
   options.results = (options.results as IUser[]).map((item) => {
     return {
       ...item,
